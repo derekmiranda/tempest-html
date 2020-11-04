f6bb2f8b7645713d6c9379cf45ce19af84983029
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-04 15:41:15 -0800

    Consts module

---
+++ src/CONSTS.ts
@@ -0,0 +1,13 @@
+import { Point } from "./types";
+
+// define geometry as relative to unit square
+export const CENTER: Point = {
+  x: 0.5,
+  y: 0.6,
+};
+
+export const COLORS = {
+  BG: "black",
+  RED: "tomato",
+  LINE: "blue",
+};
