7d86a3c61b8055228c8bf93296b3fd76e4bb2a82
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-07 23:32:30 -0800

    Adds player to level const

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -1,10 +1,13 @@
 import { Point } from "./types";
 
 // define geometry as relative to unit square
-export const CENTER: Point = {
+export const LEVEL_CENTER: Point = {
   x: 0.5,
   y: 0.6,
 };
+
+// padding b/w player and line on level w.r.t. level-space
+export const PLAYER_TO_LEVEL_DIST: number = 0.05;
 
 export const COLORS = {
   BG: "black",
