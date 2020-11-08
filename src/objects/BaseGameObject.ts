11bb6c65849887b4be65329047cbe5ed4ce750ad
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-07 22:32:48 -0800

    Global transform object

--- src/objects/BaseGameObject.ts
+++ src/objects/BaseGameObject.ts
@@ -11,18 +11,17 @@ export class BaseGameObject {
   parent: BaseGameObject;
   children: BaseGameObject[] = [];
   transform: Transform;
-  // cached global transform matrix
-  globalTransformMatrix: number[];
+  globalTransform: Transform;
 
   constructor(props: GameObjectPropsInterface) {
     Object.assign(this, props);
     this.transform = new Transform(props);
-    this.updateGlobalTransformation();
+    this.updateGlobalTransform();
   }
 
   setParent(parent: BaseGameObject) {
     this.parent = parent;
-    this.updateGlobalTransformation();
+    this.updateGlobalTransform();
   }
 
   addChildren(children: BaseGameObject[] | BaseGameObject) {
@@ -35,31 +34,39 @@ export class BaseGameObject {
 
   updateTransformWithProps(props: TransformPropsInterface) {
     this.transform.updateWithProps(props);
-    this.updateGlobalTransformation();
+    this.updateGlobalTransform();
   }
 
   // updates transformation matrix relative to whole canvas
   // and children's as well
-  updateGlobalTransformation() {
-    // update this object's global transformation matrix
-    this.globalTransformMatrix = this.transform.matrix.slice();
+  updateGlobalTransform() {
+    this.globalTransform = new Transform();
+    this.globalTransform.updateWithProps(this.transform.getTransformProps());
 
     if (this.parent) {
-      const parentGlobalMat = this.parent.globalTransformMatrix;
-      matrix.multiply(this.globalTransformMatrix, parentGlobalMat);
+      // update global transform w/ parent's global transform
+      const parentGlobalMat = this.parent.globalTransform.matrix;
+      const parentGlobalProps = this.parent.globalTransform.getTransformProps();
+      this.globalTransform.updateWithProps(parentGlobalProps);
 
       // TODO: fix parent-relative translation + rotation
       // when parent rotates, child should stay anchored w/ parent
       // however, child moves along one track when animating parent rotation
       const x = this.transform.x;
       const y = this.transform.y;
-      const newTranslateX = parentGlobalMat[0] * x + parentGlobalMat[6];
-      const newTranslateY = parentGlobalMat[4] * y + parentGlobalMat[7];
-      this.globalTransformMatrix[6] = newTranslateX;
-      this.globalTransformMatrix[7] = newTranslateY;
+      // const r = Math.sqrt(x * x + y * y);
+      // v1 = (r * cos(a1), r * sin(a1))
+      // v2 = (r * cos(a2), r * sin(a2))
+      // a2 = a1 + da
+      // v2 - v1 = (r * (cos(a2) - cos(a1)), r * )
+      const newTranslateX = parentGlobalMat[0] * x + parentGlobalProps.x;
+      const newTranslateY = parentGlobalMat[4] * y + parentGlobalProps.y;
+      this.globalTransform.matrix[6] = newTranslateX;
+      this.globalTransform.matrix[7] = newTranslateY;
     }
+
     // and children's matrices
-    for (let child of this.children) child.updateGlobalTransformation();
+    for (let child of this.children) child.updateGlobalTransform();
   }
 
   normalizePoints(relX: number, relY: number): Point {
@@ -71,7 +78,7 @@ export class BaseGameObject {
 
   localLineTo(localX: number, localY: number) {
     const resolved: Point = matrix.transformPoint(
-      this.globalTransformMatrix,
+      this.globalTransform.matrix,
       localX,
       localY
     );
@@ -81,7 +88,7 @@ export class BaseGameObject {
 
   localMoveTo(localX: number, localY: number) {
     const resolved: Point = matrix.transformPoint(
-      this.globalTransformMatrix,
+      this.globalTransform.matrix,
       localX,
       localY
     );
