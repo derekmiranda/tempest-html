e9498994aa3c84404db300997b2db69dded6bdbb
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-14 17:51:19 -0800

    Updates level consts

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -2,12 +2,15 @@ import { Point } from "./types";
 
 // define geometry as relative to unit square
 export const LEVEL_CENTER: Point = {
-  x: 0.5,
-  y: 0.6,
+  x: 0,
+  y: 0.1,
 };
 
+// scale to apply on far level shape
+export const FAR_SCALE = 0.2;
+
 // padding b/w player and line on level w.r.t. level-space
-export const PLAYER_TO_LEVEL_DIST: number = 0.05;
+export const PLAYER_TO_LEVEL_DIST: number = 0.1;
 
 export const COLORS = {
   BG: "black",
