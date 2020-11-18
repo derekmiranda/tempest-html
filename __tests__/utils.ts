ec2945c11f03677fe8abeb6a428fef7b69fcfa44
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-17 21:14:53 -0800

    Test test

---
+++ __tests__/utils.ts
@@ -0,0 +1,13 @@
+import { rotate } from "../src/utils";
+
+const TOLERANCE = 0.0001;
+
+describe("rotate", () => {
+  it("should rotate a 2D vector with given angle (radians, positive is CCW)", () => {
+    const rotated = rotate(1, 0, Math.PI / 2);
+    const isCorrectX = Math.abs(rotated.x - 0) < TOLERANCE;
+    const isCorrectY = Math.abs(rotated.y - 1) < TOLERANCE;
+    expect(isCorrectX).toBe(true);
+    expect(isCorrectY).toBe(true);
+  });
+});
