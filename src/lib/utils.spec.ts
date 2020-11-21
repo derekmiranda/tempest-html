c405cc0ecaf46261e59d5d57e61c996e01fc4171
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-20 17:05:18 -0800

    Adds midway point util

--- src/lib/utils.spec.ts
+++ src/lib/utils.spec.ts
@@ -1,6 +1,43 @@
-import { rotate } from "./utils";
+import { rotate, findPointBetweenPoints } from "./utils";
+import { Point } from "../types";
 
 const TOLERANCE = 0.0001;
+
+describe("findPointBetweenPoints", () => {
+  const start = { x: 5, y: 8 };
+  const end = { x: -4, y: 12 };
+
+  it("should return start point with k = 0", () => {
+    expect(findPointBetweenPoints(start, end, 0)).toStrictEqual(start);
+  });
+
+  it("should return end point with k = 1", () => {
+    expect(findPointBetweenPoints(start, end, 1)).toStrictEqual(end);
+  });
+
+  it("can return points with k > 1", () => {
+    expect(findPointBetweenPoints(start, end, 2)).toStrictEqual({
+      x: -13,
+      y: 16,
+    });
+  });
+
+  it("can return points with k < 0", () => {
+    expect(findPointBetweenPoints(start, end, -1)).toStrictEqual({
+      x: 14,
+      y: 4,
+    });
+  });
+
+  it("returns point midway (0 < k < 1)", () => {
+    const k = 0.3;
+    const midpoint: Point = {
+      x: start.x + k * (end.x - start.x),
+      y: start.y + k * (end.y - start.y),
+    };
+    expect(findPointBetweenPoints(start, end, k)).toStrictEqual(midpoint);
+  });
+});
 
 describe("rotate", () => {
   it("should rotate a 2D vector with given angle (radians, positive is CCW)", () => {
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,42 +1,18 @@
 import { TransformPropsInterface, Point } from "../types";
 
-// get x,y,w,h using anchor relative to width and height (in terms of 0-1)
-export function getDimsWithAnchor({
-  x,
-  y,
-  w,
-  h,
-  xAnchor,
-  yAnchor,
-}: {
-  x: number;
-  y: number;
-  xAnchor: number;
-  yAnchor: number;
-  w: number;
-  h: number;
-}): TransformPropsInterface {
-  return {
-    x: x - (1 - xAnchor) * w,
-    y: y - (1 - yAnchor) * h,
-    w,
-    h,
-  };
-}
+// math utils
+export function findPointBetweenPoints(
+  start: Point,
+  end: Point,
+  k: number
+): Point {
+  const dx = end.x - start.x;
+  const dy = end.y - start.y;
 
-// centered dims util
-export function centerDims({
-  x,
-  y,
-  w,
-  h,
-}: {
-  x: number;
-  y: number;
-  w: number;
-  h: number;
-}): TransformPropsInterface {
-  return getDimsWithAnchor({ x, y, w, h, xAnchor: 0.5, yAnchor: 0.5 });
+  return {
+    x: start.x + dx * k,
+    y: start.y + dy * k,
+  };
 }
 
 export function rotate(x, y, angle): Point {
@@ -52,6 +28,7 @@ export function rotate(x, y, angle): Point {
   };
 }
 
+// time utils
 export function throttle(fn: Function, time: number): Function {
   let lastTime: number;
   return function (...args) {
