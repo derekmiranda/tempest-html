8c834e7a87dfc1e055fb36013b698fda15cb8a40
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-23 18:15:38 -0800

    Can spawn bullets with Space

--- src/objects/Player.ts
+++ src/objects/Player.ts
@@ -1,12 +1,15 @@
 import { BaseGameObject } from "./BaseGameObject";
 import { GameObjectInterface, GameObjectPropsInterface } from "../types";
 import { COLORS } from "../CONSTS";
+import { Bullet } from "./Bullet";
+import { Level } from "./Level";
 
 interface PlayerPropsInterface extends GameObjectPropsInterface {
   color?: string;
 }
 
 export class Player extends BaseGameObject implements GameObjectInterface {
+  level: Level;
   color: string;
 
   constructor(props: PlayerPropsInterface) {
@@ -14,6 +17,31 @@ export class Player extends BaseGameObject implements GameObjectInterface {
     if (!this.color) {
       this.color = COLORS.PLAYER;
     }
+    this.keydown = this.keydown.bind(this);
+  }
+
+  keydown(e) {
+    if (e.code === "Space") {
+      const bulletTf = this.level.getBulletPath();
+      const bullet = new Bullet({
+        game: this.game,
+        ctx: this.ctx,
+        parent: this.parent,
+        ...bulletTf.to,
+        ...bulletTf,
+        w: 0.05,
+        h: 0.05,
+      });
+      this.game.addObject(bullet, this.layer);
+    }
+  }
+
+  setListeners() {
+    window.addEventListener("keydown", this.keydown, true);
+  }
+
+  removeListeners() {
+    window.removeEventListener("keydown", this.keydown, true);
   }
 
   render() {
@@ -24,5 +52,9 @@ export class Player extends BaseGameObject implements GameObjectInterface {
     this.localLineTo(-0.5, 0.5);
     this.ctx.closePath();
     this.ctx.fill();
+  }
+
+  setLevel(level) {
+    this.level = level;
   }
 }
