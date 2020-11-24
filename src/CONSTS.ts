285bb25aedaf6a9fe5b8f3b03a634dd6988afc47
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-24 11:20:47 -0800

    Adds COLLISION_TOLERANCE

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -10,9 +10,12 @@ export const LEVEL_CENTER: Point = {
 export const FAR_SCALE = 0.1;
 
 // padding b/w player and line on level w.r.t. level-space
-export const PLAYER_TO_LEVEL_DIST: number = 0;
-export const PLAYER_TO_LEVEL_SIZE: number = 0.2;
-export const ENEMY_TO_LEVEL_SIZE: number = 0.2;
+export const PLAYER_TO_LEVEL_DIST = 0;
+export const PLAYER_TO_LEVEL_SIZE = 0.2;
+export const ENEMY_TO_LEVEL_SIZE = 0.2;
+
+// z-value tolerance for collisions
+export const COLLISION_TOLERANCE = 0.01;
 
 export const COLORS = {
   BG: "black",
@@ -22,6 +25,7 @@ export const COLORS = {
   LINE: "blue",
 };
 
+export const RED_ENEMY_SPEED = 0.0002;
 export const BULLET_SPEED = 0.003;
 
 export const MAX_ID = Number.MAX_SAFE_INTEGER;
