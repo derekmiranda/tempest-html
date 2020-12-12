92124642352cf58ede1afddc5d4b8e0462c45052
Derek Miranda <derek.miranda.1992@gmail.com>
2020-12-11 17:04:53 -0800

    Extracts Red enemy-specific methods

--- src/objects/RedEnemy.ts
+++ src/objects/RedEnemy.ts
@@ -1,8 +1,9 @@
 import { Point } from "../types";
-import { COLORS, RED_ENEMY_SPEED } from "../CONSTS";
+import { COLORS, LEVEL_CENTER } from "../CONSTS";
 import { Level } from "./Level";
 import { Enemy, EnemyPropsInterface } from "./Enemy";
 import { findPointBetweenPoints, sleep } from "../lib/utils";
+import { farDot } from "../lib/shapes";
 
 export class RedEnemy extends Enemy {
   color: string = COLORS.RED;
@@ -29,6 +30,15 @@ export class RedEnemy extends Enemy {
       { x: 0.25, y: 0 },
       { x: 0.5, y: 0.25 },
     ];
+  }
+
+  calcFarPoint(): Point {
+    // range from 0.2 to 0.8
+    const distAwayFromLine = 0.8 - 0.6 * Math.min(1, this.transform.z - 1);
+    return {
+      x: LEVEL_CENTER.x + (this.to.x - LEVEL_CENTER.x) * distAwayFromLine,
+      y: LEVEL_CENTER.y + (this.to.y - LEVEL_CENTER.y) * distAwayFromLine,
+    };
   }
 
   update(timeDelta: number) {
@@ -58,6 +68,24 @@ export class RedEnemy extends Enemy {
     } else {
       this.moving = false;
     }
+  }
+
+  render() {
+    this.ctx.strokeStyle = this.color;
+    this.ctx.beginPath();
+    // render far shape
+    if (this.transform.z > 1) {
+      const farPoints = farDot(this.calcFarPoint());
+      farPoints.forEach(({ x, y }, i) => {
+        i === 0 ? this.level.localMoveTo(x, y) : this.level.localLineTo(x, y);
+      });
+    } else {
+      this.points.forEach(({ x, y }, i) => {
+        i === 0 ? this.localMoveTo(x, y) : this.localLineTo(x, y);
+      });
+    }
+    this.ctx.closePath();
+    this.ctx.stroke();
   }
 
   setRenderedState(state: boolean) {
